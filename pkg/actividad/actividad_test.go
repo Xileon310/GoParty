bda6ff2b97727b283d071a581e1d195eaa5b92eb
Xileon310 <joseluis404@correo.ugr.es>
2021-12-10 21:11:27 +0100

    Se corrige error sintáctico. Avanza #27

--- pkg/actividad/actividad_test.go
+++ pkg/actividad/actividad_test.go
@@ -7,22 +7,22 @@ import (
 // ---------------------- Tests del Objeto Valor Zona ----------------------
 func TestLocalidadNoVacia(t *testing.T) {
 	_, err := NewZona("", "Granada", "España")
-	if err != nil {
-		t.Errorf("Localidad no válida en objeto Zona")
+	if err == nil {
+		t.Fatalf("Localidad no válida en objeto Zona")
 	}
 }
 
 func TestProvinciaNoVacia(t *testing.T) {
 	_, err := NewZona("Motril", "", "España")
-	if err != nil {
-		t.Errorf("Provincia no válida en objeto Zona")
+	if err == nil {
+		t.Fatalf("Provincia no válida en objeto Zona")
 	}
 }
 
 func TestPaisNoVacio(t *testing.T) {
 	_, err := NewZona("Motril", "Granada", "")
-	if err != nil {
-		t.Errorf("País no válido en objeto Zona")
+	if err == nil {
+		t.Fatalf("País no válido en objeto Zona")
 	}
 }
 
@@ -30,15 +30,15 @@ func TestPaisNoVacio(t *testing.T) {
 func TestTituloNoVacio(t *testing.T) {
 	zona := Zona{"Motril", "Granada", "España"}
 	_, err := NewActividad("", zona, Ocio)
-	if err != nil {
-		t.Errorf("Título no válido en objeto Actividad")
+	if err == nil {
+		t.Fatalf("Título no válido en objeto Actividad")
 	}
 }
 
 func TestCategoriaNoValida(t *testing.T) {
 	zona := Zona{"Motril", "Granada", "España"}
 	_, err := NewActividad("Ruta de senderismo", zona, 99)
-	if err != nil {
-		t.Errorf("Categoría no válida en objeto Actividad")
+	if err == nil {
+		t.Fatalf("Categoría no válida en objeto Actividad")
 	}
 }
