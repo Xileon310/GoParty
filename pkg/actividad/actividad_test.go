2ed15d2e3aae1c20a707ef9adf10dfd2ea61cc67
Xileon310 <joseluis.alfaix@gmail.com>
2021-12-16 13:52:02 +0100

    Cambia tests. Closes #28 #29 #30 #31 #32
    Los anteriores tests no estaban bien implementados.
    Se ha derivado la captura del error a la clase correspondiente.
    En los test se mostrará el mensaje de error generado en la clase
    correspondiente.

--- pkg/actividad/actividad_test.go
+++ pkg/actividad/actividad_test.go
@@ -5,40 +5,18 @@ import (
 )
 
 // ---------------------- Tests del Objeto Valor Zona ----------------------
-func TestLocalidadNoVacia(t *testing.T) {
-	_, err := NewZona("", "Granada", "España")
-	if err == nil {
-		t.Fatalf("Localidad no válida en objeto Zona")
-	}
-}
-
-func TestProvinciaNoVacia(t *testing.T) {
-	_, err := NewZona("Motril", "", "España")
-	if err == nil {
-		t.Fatalf("Provincia no válida en objeto Zona")
-	}
-}
-
-func TestPaisNoVacio(t *testing.T) {
-	_, err := NewZona("Motril", "Granada", "")
-	if err == nil {
-		t.Fatalf("País no válido en objeto Zona")
+func TestZonaValida(t *testing.T) {
+	_, err := NewZona("Motril", "Granada", "España")
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
 }
 
 // ---------------------- Tests de la Entidad Actividad ----------------------
-func TestTituloNoVacio(t *testing.T) {
+func TestActividadValida(t *testing.T) {
 	zona := Zona{"Motril", "Granada", "España"}
-	_, err := NewActividad("", zona, Ocio)
-	if err == nil {
-		t.Fatalf("Título no válido en objeto Actividad")
-	}
-}
-
-func TestCategoriaNoValida(t *testing.T) {
-	zona := Zona{"Motril", "Granada", "España"}
-	_, err := NewActividad("Ruta de senderismo", zona, 99)
-	if err == nil {
-		t.Fatalf("Categoría no válida en objeto Actividad")
+	_, err := NewActividad("Ruta de senderismo", zona, Ocio)
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
 }
